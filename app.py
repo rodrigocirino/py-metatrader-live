57f9b1cd08074d66a7ff8edc91ca217e15b7e49a
rodrigocirino <rodrigorcandrade@gmail.com>
2024-07-10 21:22:58 -0300

    update

--- app.py
+++ app.py
@@ -24,17 +24,15 @@ class TickReceiver:
         self.scheduler.enter(self.interval, 1, self.process_ticks)
 
     def load_until_now(self):
-        num_bars = 130  # número de barras de dados para recuperar
+        num_bars = 114  # número de barras de dados para recuperar
         # Obter os dados OHLC
         bars = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M5, 0, num_bars)
         df = pd.DataFrame(bars)
         df['time'] = pd.to_datetime(df['time'], unit='s')
         # Definir a coluna 'time' como índice
         df.set_index('time', inplace=True)
-        print(df[['open','high','low','close','tick_volume']])
+        print(df[['open', 'high', 'low', 'close', 'tick_volume']])
         self.df = df
-
-
 
     def process_ticks(self):
         symbol_data = mt5.symbol_info(self.symbol)
@@ -44,15 +42,29 @@ class TickReceiver:
         if symbol_data is None:
             print(f"Não foi possível obter informações sobre o símbolo {self.symbol}")
         else:
+            # PROCESS
             self.update_dataframe(symbol_data)
+            self.calculate_atr()
+            # SYSOUT
+            self.print_dataframe()
         self.scheduler.enter(self.interval, 1, self.process_ticks)
+
+    def print_dataframe(self):
+        # print(self.df.columns)
+        dff = self.df.loc[(self.df.index >= pd.Timestamp("2024-07-10 10:15:00"))]
+        dff = dff.drop(columns=['tick_volume', 'spread', 'real_volume'], errors='ignore')
+        print(dff)
 
     def update_dataframe(self, symbol_data):
         stime = pd.to_datetime(symbol_data.time, unit='s')
         new_record = pd.DataFrame({'close': symbol_data.last}, index=[stime])
         self.df = pd.concat([self.df, new_record])
-        print(self.df[['open', 'high', 'low', 'close', 'tick_volume']])
+        self.df = self.df[~self.df.index.duplicated(keep='last')]  # Remove duplicate times, keep the last
 
+    def calculate_atr(self):
+        """Calculates the ATR (Average True Range) for the last 20 bars."""
+        self.df['atr'] = abs(self.df['open'] - self.df['close'])
+        self.df['atr'].tail(20).mean()
 
     @staticmethod
     def finalize():
@@ -72,8 +84,4 @@ if __name__ == "__main__":
     tick_receiver = TickReceiver(symbol="WIN$D", interval=3)
     tick_receiver.initialize()
     tick_receiver.load_until_now()
-    print(f"{'Time'.ljust(20)} {'Ticker'.ljust(8)} {'Last'.ljust(10)} {'Volume'.ljust(12)}".upper())
-    tick_receiver.run()
-
-# df.columns
-# Index(['open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume'], dtype='object')
+    tick_receiver.run()  # infinite loop
