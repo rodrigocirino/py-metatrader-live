dc2be0e878639d19a911f2d952698b46aa450067
rodrigocirino <rodrigorcandrade@gmail.com>
2024-07-10 22:49:33 -0300

    update

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class TickReceiver:
         self.scheduler.enter(self.interval, 1, self.process_ticks)
 
     def load_until_now(self):
-        num_bars = 114  # número de barras de dados para recuperar
+        num_bars = 1000  # número de barras de dados para recuperar
         # Obter os dados OHLC
         bars = mt5.copy_rates_from_pos(self.symbol, mt5.TIMEFRAME_M5, 0, num_bars)
         df = pd.DataFrame(bars)
@@ -41,16 +41,18 @@ class TickReceiver:
             # PROCESS
             self.update_dataframe(symbol_data)
             self.calculate_atr()
+            self.is_atr_over()
             # SYSOUT
             self.print_dataframe()
         self.scheduler.enter(self.interval, 1, self.process_ticks)
 
     def print_dataframe(self):
-        # print(self.df.columns)
         self.df.drop(columns=['tick_volume', 'spread', 'real_volume'], errors='ignore', inplace=True)
         # self.df = self.df.loc[(self.df.index >= pd.Timestamp("2024-07-10 10:15:00"))]
         pd.set_option('display.max_columns', None)  # Ensure all columns are printed
-        print(self.df.head(60))
+        print(self.df)
+        print("> Barras com ATR 20 Superior a 1.5 vezes.")
+        print(self.df.loc[self.df.is_over].tail(115))
 
     def update_dataframe(self, symbol_data):
         stime = pd.to_datetime(symbol_data.time, unit='s')
@@ -60,23 +62,15 @@ class TickReceiver:
 
     def calculate_atr(self):
         """Calculates the ATR (Average True Range) for the last 20 bars."""
-        '''
-        self.df['atr'] = abs(self.df['open'] - self.df['close'])
-        self.df['atr'].rolling(window=20).mean()
-        self.df['atr_i'] = self.df['atr'].shift()
-        self.df['atr_b'] = self.df['atr'] > self.df['atr_i'] * 2
-        self.df.drop(columns=['atr_i'], errors='ignore', inplace=True)
-        print(self.df.loc[self.df.atr_b].head(60))
-        '''
-        """Calculates the ATR (Average True Range) for the last 20 bars."""
-        self.df['High-Low'] = self.df['high'] - self.df['low']
-        self.df['High-PreviousClose'] = abs(self.df['high'] - self.df['close'].shift(1))
-        self.df['Low-PreviousClose'] = abs(self.df['low'] - self.df['close'].shift(1))
+        dff = pd.DataFrame()
+        dff['High-Low'] = self.df['high'] - self.df['low']
+        dff['High-PreviousClose'] = abs(self.df['high'] - self.df['close'].shift(1))
+        dff['Low-PreviousClose'] = abs(self.df['low'] - self.df['close'].shift(1))
+        dff['TrueRange'] = dff[['High-Low', 'High-PreviousClose', 'Low-PreviousClose']].max(axis=1)
+        self.df['true_range'] = dff['TrueRange'].rolling(window=20).mean()
 
-        self.df['TrueRange__'] = self.df[['High-Low', 'High-PreviousClose', 'Low-PreviousClose']].max(axis=1)
-        self.df['true_range'] = self.df['TrueRange__'].rolling(window=20).mean()
-        self.df.drop(columns=['High-Low', 'High-PreviousClose', 'Low-PreviousClose', 'TrueRange__'],
-                               errors='ignore', inplace=True)
+    def is_atr_over(self):
+        self.df['is_over'] = abs(self.df['open'] - self.df['close']) > (self.df['true_range'].shift() * 1.5)
 
     @staticmethod
     def finalize():
