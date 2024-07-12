92d62e5e48f7fffd8037ac241d3061f79cfadddd
rodrigocirino <rodrigorcandrade@gmail.com>
2024-07-12 12:19:54 -0300

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import MetaTrader5 as mt5
 import pandas as pd
+import numpy as np
 
 
 class TickReceiver:
@@ -42,6 +43,7 @@ class TickReceiver:
             self.update_dataframe(symbol_data)
             self.calculate_atr()
             self.is_atr_over()
+            self.ema()
             # SYSOUT
             self.print_dataframe()
         self.scheduler.enter(self.interval, 1, self.process_ticks)
@@ -51,8 +53,9 @@ class TickReceiver:
         # self.df = self.df.loc[(self.df.index >= pd.Timestamp("2024-07-10 10:15:00"))]
         pd.set_option('display.max_columns', None)  # Ensure all columns are printed
         print(self.df)
-        print("> Barras com ATR 20 Superior a 1.5 vezes.")
-        print(self.df.loc[self.df.is_over].tail(115))
+        # print("> Barras com ATR 20 Superior a 1.5 vezes.")
+        # print(self.df.loc[self.df.is_over].tail(115))
+        print((self.df.loc[:, ~self.df.columns.isin(['open', 'high', 'low'])]).tail(40))
 
     def update_dataframe(self, symbol_data):
         stime = pd.to_datetime(symbol_data.time, unit='s')
@@ -60,14 +63,25 @@ class TickReceiver:
         self.df = pd.concat([self.df, new_record])
         self.df = self.df[~self.df.index.duplicated(keep='last')]  # Remove duplicate times, keep the last
 
-    def calculate_atr(self):
-        """Calculates the ATR (Average True Range) for the last 20 bars."""
+    def calculate_atr(self, n=14):
+        """Calculates the ATR (Average True Range) for the last n bars."""
+        # Wilder quer calcular a amplitude maior, se a minima for mais distante que a maxima do fechamento anterior use esta, senao a outra
+        #  ATR = MME(máx(H – L, H – Cp, Cp – L)) - Cp=previous close
         dff = pd.DataFrame()
-        dff['High-Low'] = self.df['high'] - self.df['low']
-        dff['High-PreviousClose'] = abs(self.df['high'] - self.df['close'].shift(1))
-        dff['Low-PreviousClose'] = abs(self.df['low'] - self.df['close'].shift(1))
-        dff['TrueRange'] = dff[['High-Low', 'High-PreviousClose', 'Low-PreviousClose']].max(axis=1)
-        self.df['true_range'] = dff['TrueRange'].rolling(window=20).mean()
+
+        dff['H-L'] = self.df['high'] - self.df['low']
+        dff['|H-Cp|'] = np.abs(self.df['high'] - self.df['close'].shift(1))
+        dff['|L-Cp|'] = np.abs(self.df['low'] - self.df['close'].shift(1))
+        dff['TR'] = dff[["|H-Cp|", "H-L", "|L-Cp|"]].max(axis=1)
+        # 
+        sma = dff['TR'].rolling(window=n, min_periods=n).mean()[:n]
+        rest = dff['TR'][n:]
+        dff['true_range'] = pd.concat([sma, rest]).ewm(alpha=1 / n, adjust=False).mean()  # Calculate MME
+        self.df['true_range'] = dff['true_range'].round(decimals=2)
+        # Número máximo de ações permitidas = shares = total_capital * risk / df['ATR'].iloc[-1] # total_capital = 20000, risk = 0.005
+
+    def ema(self, s=20):
+        self.df["EMA" + str(s)] = self.df['close'].ewm(span=s).mean().round(decimals=0)
 
     def is_atr_over(self):
         self.df['is_over'] = abs(self.df['open'] - self.df['close']) > (self.df['true_range'].shift() * 1.5)
