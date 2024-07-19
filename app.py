e68aadda3f6812eddbddef9929493444a90bc5d8
rodrigocirino <rodrigorcandrade@gmail.com>
2024-07-18 21:30:10 -0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 import pandas as pd
-import numpy as np
 
 from service.mt5_service import MT5_Service
+from util.indicators.command import CommandController
 from util.indicators.ema import Ema
 from util.indicators.true_range import TrueRange
 from util.scheduler import scheduler
@@ -21,13 +21,16 @@ class TickReceiver:
         self.from_date = datetime.now()
         self.rates = MT5_Service(servicemanager)
 
-    def print_dataframe(self):
-        self.df.drop(columns=['tick_volume', 'spread', 'real_volume'], errors='ignore', inplace=True)
-        pd.set_option('display.max_columns', None)  # Ensure all columns are printed
-        print((self.df.loc[:, ~self.df.columns.isin(['open', 'high', 'low'])]).tail(40))
-        # self.df = self.df.loc[(self.df.index >= pd.Timestamp("2024-07-10 10:15:00"))]
-        # print("> Barras com ATR 20 Superior a 1.5 vezes.")
-        # print(self.df.loc[self.df.is_over].tail(115))
+    def run(self):
+        try:
+            if self.rates.initialize():
+                self.scheduler.renew(self.process_ticks())
+            else:
+                print("Falha na inicialização do serviço.")
+        except KeyboardInterrupt:
+            print("Interrupção pelo usuário. Encerrando o programa...")
+        finally:
+            self.rates.finalize()
 
     def mining_dataframe(self, bars):
         self.df = pd.DataFrame(bars)
@@ -42,29 +45,34 @@ class TickReceiver:
         else:
             self.mining_dataframe(bars)
 
-            dtr = TrueRange(self.df)
-            dtr.execute()
+            self.analyze_indicators()
 
-            dema = Ema(self.df)
-            dema.execute()
-
-            # print("\n =========== XXXXXXXXXXXXXXX =================\n")
             self.print_dataframe()
         # Update Scheduler
         self.scheduler.renew(self.process_ticks)
 
-    def run(self):
-        try:
-            if self.rates.initialize():
-                self.scheduler.renew(self.process_ticks())
-            else:
-                print("Falha na inicialização do serviço.")
-        except KeyboardInterrupt:
-            print("Interrupção pelo usuário. Encerrando o programa...")
-        finally:
-            self.rates.finalize()
+    def print_dataframe(self):
+        import os
+        log_filepath = "util/export"
+        os.makedirs(log_filepath, exist_ok=True)
+        self.df.to_csv(f"{log_filepath}/console_{servicemanager}.csv")  # , sep="\t", decimal=',')
+        self.df.drop(columns=['tick_volume', 'spread', 'real_volume'], errors='ignore', inplace=True)
+        pd.set_option('display.max_columns', None)  # Ensure all columns are printed
+        print((self.df.loc[:, ~self.df.columns.isin(['open', 'high', 'low'])]).tail(40))
+        # self.df = self.df.loc[(self.df.index >= pd.Timestamp("2024-07-10 10:15:00"))]
+        # print("> Barras com ATR 20 Superior a 1.5 vezes.")
+        # print(self.df.loc[self.df.is_over].tail(115))
+
+    def analyze_indicators(self):
+        # Cria uma instância do controlador
+        controller = CommandController()
+        # Adiciona os comandos ao controlador
+        controller.add_command(TrueRange(self.df))
+        controller.add_command(Ema(self.df))
+        # Processa os comandos
+        controller.process_command()
 
 
 if __name__ == "__main__":
-    tick_receiver = TickReceiver(symbol="EURUSD", interval=5)
+    tick_receiver = TickReceiver(symbol="EURUSD", interval=60)
     tick_receiver.run()  # infinite loop
